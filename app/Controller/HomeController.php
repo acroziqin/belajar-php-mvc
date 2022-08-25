4e6238770a2c5d69633cfeb7ad9a7219d5f575cf
unknown <achmadchoirurroziqin@gmail.com>
2022-08-25 18:52:31 +0700

    Model

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,6 +6,10 @@ class HomeController
 {
     function index(): void
     {
+        $model = [
+            "title" => "Belanja Skincare",
+            "content" => "Selamat berbelanja skincare Krisna Beaute"
+        ];
         echo "HomeController.index()";
     }
 
@@ -22,5 +26,22 @@ class HomeController
     function about(): void
     {
         echo "Author : Achmad Choirur Roziqin";
+    }
+
+    function login(): void
+    {
+        $request = [
+            "username" => $_POST['username'],
+            "password" => $_POST['password']
+        ];
+
+        $user = [
+
+        ];
+
+        $response = [
+            "message"=>"Login Sukses"
+        ];
+        // Kirimkan response ke view
     }
 }
